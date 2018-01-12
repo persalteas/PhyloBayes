5617a9227a2fa5b3404652c39e927a38eb192d44
WaitndSee <antoine.weber@insa-lyon.fr>
2018-01-12 17:04:48 +0100

    Update plot_results.R

--- plot_results.R
+++ plot_results.R
@@ -6,3 +6,10 @@ d = read.table(paste(filename,".trace", sep= ""), h=T)
 par(mfrow=c(2,1))
 plot(d$rate[chauffe:length(d$rate)], type = 'l', xlab = "iterations", ylab = "rate u")
 plot(d$lnL[chauffe:length(d$lnL)], type = 'l', xlab = "iterations", ylab = "logLikelihood", col = 2)
+
+
+rawdata = read.table(file="ch1.distH",header = F)
+distH = as.numeric(as.vector(rawdata$V1))
+ageAncetre = (1/distH)*6
+hist(ageAncetre,breaks=20,col="darkorange1",main="Age estime de l'ancetre commun")
+
